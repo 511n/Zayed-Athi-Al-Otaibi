a975455c39dd08fd3fe791c2cb804c26baf4f698
Zayed ALotaibi <munif.ofoh@gmail.com>
2026-05-17 18:26:33 +0300

    Add Files

---
+++ week4-HW/Problem7.py
@@ -0,0 +1,22 @@
+age = int(input("Enter your age: "))
+income = int(input("Enter your monthly income: "))
+job = input("Do you have a job? (yes/no): ")
+
+if age >= 21 and age <= 65:
+
+    if job == "yes":
+
+        if income >= 5000:
+            print("Approved")
+
+        elif income >= 3000:
+            print("Approved with conditions")
+
+        else:
+            print("Rejected: low income")
+
+    else:
+        print("Rejected: no job")
+
+else:
+    print("Rejected: age not eligible")
