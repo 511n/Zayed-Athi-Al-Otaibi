50d86d52bf7b17538f5bbe4528bc9e6cb176d796
Zayed ALotaibi <munif.ofoh@gmail.com>
2026-06-04 00:26:10 +0300

    delete one files

--- ProblemS1.py
+++
@@ -1,8 +0,0 @@
-colors = {"red", "blue", "green"}
-
-colors.add("yellow")
-colors.add("red")
-
-print("Size:", len(colors))
-print("red in set:", "red" in colors)
-print("yellow in set:", "yellow" in colors)
