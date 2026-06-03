c95c95f8eade9fe5dafb7b8900aa24631dcfb403
Zayed ALotaibi <munif.ofoh@gmail.com>
2026-06-04 00:24:20 +0300

    delete one files

--- ProblemD3.py
+++
@@ -1,9 +0,0 @@
-user = {
-    "name": "Sara",
-    "email": "sara@example.com",
-    "city": "Jeddah"
-}
-
-print("Keys:", list(user.keys()))
-print("'name' in dict:", "name" in user)
-print("'phone' in dict:", "phone" in user)
